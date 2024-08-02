b838c5595dfcc4abefbdf40cce4b0deeff3c1a94
Nour <nourpow@gmail.com>
2024-08-02 10:19:11 +0100

    update

--- components/auth/LoginForum.tsx
+++ components/auth/LoginForum.tsx
@@ -48,6 +48,7 @@ export const LoginForum = () => {
                       type="email"
                     />
                   </FormControl>
+                  <FormMessage />
                 </FormItem>
               )}
             />
