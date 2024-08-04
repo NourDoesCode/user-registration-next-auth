9210d2504ad8bdc720dfb2b872a6cc8265edd39b
Nour <nourpow@gmail.com>
2024-08-04 07:21:40 +0100

    registering a new user setup

--- actions/register.ts
+++ actions/register.ts
@@ -1,11 +1,34 @@
 "use server";
 import * as z from "zod";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields.success) {
     return { error: "Invalid credentials!" };
   }
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  //make sure the email is not taken
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (existingUser) {
+    return { error: "This email is taken" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  //TODO :Send verification token email
   return { success: "Logged in successfully" };
 };
